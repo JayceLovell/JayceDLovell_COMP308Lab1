62ba48ffbbeed4d34194c4b5856a23247226bb9a
Jayce D Lovell <jaycedlovell@gmail.com>
2019-01-29 14:53:58 -0500

    Completed assignment

--- JayceDLovell_COMP308Lab1/app/controllers/feedback.server.controller.js
+++ JayceDLovell_COMP308Lab1/app/controllers/feedback.server.controller.js
@@ -2,15 +2,18 @@
     console.log("in feedback controller");
     let email = request.body.email;
     let firstname = request.body.firstname;
-    console.log("email on feedback " + JSON.stringify(email));
-    console.log("firstname on feedback " + JSON.stringify(firstname));
+    let lastname = request.body.lastname;
+    //let comments = request.body.comments;
+    //let numberOfLanguages = request.body.numberOfLanguages;
+    //console.log("email on feedback " + JSON.stringify(email));
+    //console.log("firstname on feedback " + JSON.stringify(firstname));
     //Document.getElementById("email").innerText = JSON.stringify(email);
     //request.body.email.innerHTML = JSON.stringify(email);
-    if (firstname) {
-        let email = request.body.email;
-        let firstname = request.body.firstname;
-        console.log("email on  leaving feedback " + JSON.stringify(email));
-        console.log("firstname on leaving feedback " + JSON.stringify(firstname));
+    if (firstname && lastname) {
+        //let email = request.body.email;
+        //let firstname = request.body.firstname;
+        //console.log("email on  leaving feedback " + JSON.stringify(email));
+        //console.log("firstname on leaving feedback " + JSON.stringify(firstname));
         response.render('ThankYou');
     }
     else
--- JayceDLovell_COMP308Lab1/server.js
+++ JayceDLovell_COMP308Lab1/server.js
@@ -2,5 +2,5 @@ process.env.NODE_ENV = 'development'; //process.node.NODE_ENV || 'development';
 const configureExpress = require('./config/express')
 const app = configureExpress();
 app.listen(1337);
-console.log("Server is running at http://localhost:3000/");
+console.log("Server is running at http://localhost:1337/");
 module.exports = app;
